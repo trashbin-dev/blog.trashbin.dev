1426a35e37b4ecc2a5f086eb5869aab3cb81fc62
Ogromny <ogromnycoding@gmail.com>
2021-09-29 10:43:10 +0200

    feat(pages: author): add last posts

--- components/ArticleLabel.vue
+++ components/ArticleLabel.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex gap-x-4">
         <NuxtLink :to="url" class="flex-1">{{article.title}}</NuxtLink>
-        <AuthorLabel :slug="article.author" />
+        <AuthorLabel v-if="!authorless" :slug="article.author" />
         <span>{{article.createdAt | date}}</span>
     </div>
 </template>
@@ -14,6 +14,10 @@ export default {
         article: {
             type: Object,
             required: true
+        },
+        authorless: {
+            type: Boolean,
+            default: false
         }
     },
     computed: {
--- pages/author/_slug.vue
+++ pages/author/_slug.vue
@@ -1,12 +1,24 @@
 <template>
-    <div class="container mx-auto">
-        <div v-if="author" class="flex gap-x-4 items-center p-5 rounded-full shadow-md">
+    <div class="container mx-auto" v-if="author">
+        <div class="flex gap-x-4 items-center p-5 rounded-full shadow-md">
             <span class="text-4xl p-5 rounded-full shadow-inner bg-gray-50">{{author.icon}}</span>
             <div class="flex flex-col justify-between">
                 <span class="text-xl text-gray-700">{{author.name}}</span>
                 <span class="text-sm text-gray-600">{{author.desc}}</span>
             </div>
         </div>
+
+        <template v-if="articles">
+            <span class="mt-8 block text-xl font-bold text-gray-800">Posts</span>
+            <div class="flex flex-col mt-4">
+                <ArticleLabel 
+                    v-for="article of articles"
+                    :key ="article.slug"
+                    :article="article"
+                    authorless
+                />
+            </div>
+        </template>
     </div>
 </template>
 
@@ -18,11 +30,16 @@ export default {
             .fetch()
             .catch((err) => /* TODO: */ undefined)
 
-        if (author) {
-            return {author: Array.isArray(author) ? author[0] : author}
+        if (!author) {
+            return {author: undefined}
         }
 
-        return {author: undefined}
+        const articles = await $content("articles")
+            .where({author: params.slug})
+            .fetch()
+            .catch(err => /* TODO: */ undefined)
+
+        return {author: Array.isArray(author) ? author[0] : author, articles}
     }
 }
 </script>
